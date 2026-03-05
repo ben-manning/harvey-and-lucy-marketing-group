805f02007a0eb336be1e7d1705405b88a56dac2f
Ben Manning <bmanning@me.com>
2026-03-05 13:07:15 -0500

    more style adjustments and footer change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { navbar } from '../partials/navbar.js';
+import { footer } from '../partials/footer.js';
 
 document.getElementById('navbar').outerHTML = navbar;
+document.getElementById('footer').outerHTML = footer;
---
+++ partials/footer.js
@@ -0,0 +1,9 @@
+export const footer = `
+<footer class="site-footer">
+  <p>&copy; 2026 Harvey &amp; Lucy Growth Partners. All rights reserved.</p>
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="contact.html">Contact</a>
+  </nav>
+</footer>`;
