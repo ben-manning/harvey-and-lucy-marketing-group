f228e44ca254cce8794ae0fcd48cffd1ea375b13
Ben Manning <bmanning@me.com>
2026-03-05 11:57:40 -0500

    formatting the site

--- partials/navbar.js
+++ partials/navbar.js
@@ -1,7 +1,9 @@
 export const navbar = `
 <header>
   <nav>
-    <a href="index.html" class="logo">Harvey &amp; Lucy Growth Partners</a>
+    <a href="index.html" class="logo">
+      <img src="assets/images/monogram.svg" alt="Harvey &amp; Lucy Growth Partners">
+    </a>
     <ul>
       <li><a href="index.html">Home</a></li>
       <li><a href="about.html">About</a></li>
